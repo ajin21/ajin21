254448643e83553709397b6de51df71c1528bb7e
ajin21 <72785648+ajin21@users.noreply.github.com>
2024-07-16 18:39:56 +0800

    Update source.js

--- assets/js/source.js
+++ assets/js/source.js
@@ -6,6 +6,19 @@ var navData = {
             icon: "history",
             nav: []
         },
+	 {
+            title: "购物平台",
+            icon: "shopping-cart",
+            nav:[
+                {
+                    title:"什么值得买",
+                    desc:"张大妈你值得拥有",
+                    url:"https://www.smzdm.com/",
+                    image:"smzdm.png"
+                },
+            ]
+        }
+   
 		
 		
         
