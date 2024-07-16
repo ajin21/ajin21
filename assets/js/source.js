a9ab92da01656ecbbcf3e55cf8aff463c597e8d3
ajin21 <72785648+ajin21@users.noreply.github.com>
2024-07-16 18:45:27 +0800

    Update source.js

--- assets/js/source.js
+++ assets/js/source.js
@@ -6,7 +6,9 @@ var navData = {
             icon: "history",
             nav: []
         },
-	 {
+   	
+        //购物平台
+        {
             title: "购物平台",
             icon: "shopping-cart",
             nav:[
@@ -16,11 +18,37 @@ var navData = {
                     url:"https://www.smzdm.com/",
                     image:"smzdm.png"
                 },
+                {
+                    title:"逛丢",
+                    desc:"实时同步全网折扣",
+                    url:"https://guangdiu.com/",
+                    image:"guangdiu.png"
+                },
+                {
+                    title:"京东商城",
+                    desc:"支持东哥",
+                    url:"https://jd.com",
+                    image:"jd.png"
+                },
+                {
+                    title:"淘宝",
+                    desc:"不解释",
+                    url:"https://taobao.com",
+                    image:"taobao.png"
+                },
+                {
+                    title:"京东联盟",
+                    desc:"京东返利网站",
+                    url:"https://union.jd.com/",
+                    image:"jd.png"
+                },
+                {
+                    title:"阿里妈妈",
+                    desc:"淘宝返利网站",
+                    url:"https://www.alimama.com/",
+                    image:"alimama.png"
+                },
             ]
-        }
-   
-		
-		
-        
+        },
     ]
 }
